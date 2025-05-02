b588f99c18d67f4612c94d604247497a42b83780
DarkCommit <gabrielraialsantos@gmail.com>
2025-05-02 14:31:01 -0300

    Initial commit

---
+++ src/main/java/ccom/darkcommit/parking_control/ParkingControlApplication.java
@@ -0,0 +1,13 @@
+package ccom.darkcommit.parking_control;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingControlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ccom/darkcommit/parking_control/ParkingControlApplicationTests.java
@@ -0,0 +1,13 @@
+package ccom.darkcommit.parking_control;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
